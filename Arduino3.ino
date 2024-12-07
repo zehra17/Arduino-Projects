7f5e9c2d52307dee0108d7323aa6253c17559902
zehra17 <zy718462@gmail.com>
2024-12-07 20:19:46 +0300

    Add files via upload

---
+++ Arduino3.ino
@@ -0,0 +1,16 @@
+#define potpin A0
+int deger=0;
+
+void setup() {
+
+Serial.begin(9600);
+Serial.println("Pot Değer Okuma");
+
+}
+
+void loop() {
+
+deger = analogRead(potpin);
+Serial.println(deger);
+
+}
